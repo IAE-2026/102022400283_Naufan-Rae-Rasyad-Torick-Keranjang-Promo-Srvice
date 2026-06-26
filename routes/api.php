ca5ba7a18f4c0fa377aca73153db0e63ddf5f26c
Naufanrae <naufan.torick@gmail.com>
2026-06-26 17:34:34 +0700

    chore: update routes

--- routes/api.php
+++ routes/api.php
@@ -5,21 +5,8 @@ use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\PromoController;
 use App\Http\Controllers\Api\AuthController;
 
-// Auth routes (public)
-Route::prefix('v1/auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-});
-
-// Protected auth routes
-Route::prefix('v1/auth')->middleware('jwt.auth')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-});
-
 // Protected cart & promo routes
 Route::prefix('v1')->middleware('apikey')->group(function () {
-
-    Route::get('/products', [CartController::class, 'products']);
-
     Route::post('/carts', [CartController::class, 'store']);
 
     Route::delete('/carts/{id}', [CartController::class, 'destroy']);
@@ -31,4 +18,18 @@ Route::prefix('v1')->middleware('apikey')->group(function () {
     Route::get('/promo/{id}', [PromoController::class, 'show']);
 
     Route::post('/promo/apply', [PromoController::class, 'apply']);
+    Route::get('/products', [CartController::class, 'products']);
+
+   
 });
+
+// Auth routes (public)
+Route::prefix('v1/auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+// Protected auth routes
+Route::prefix('v1/auth')->middleware('jwt.auth')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+});
+
